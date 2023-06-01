f1c36896f99bcf57dc0a8643abc266316a0a13ac
danielloaizan01 <61980060+danielloaizan01@users.noreply.github.com>
2023-06-01 18:20:45 -0500

    Update environment.ts

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   route: 'http://fabricaescuelamodgestionnotas-production.up.railway.app/api/V1/',
   urlEndPoint: 'http://fabricaescuelamodgestionnotas-production.up.railway.app/api/V1/',
   cookieToken: 'token'
