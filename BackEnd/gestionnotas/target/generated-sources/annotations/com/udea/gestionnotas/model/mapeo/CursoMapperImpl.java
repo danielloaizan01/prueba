0bb9d9ea531c027547b23c48ff2aa86368e9e80e
Samuelab2007 <90534570+Samuelab2007@users.noreply.github.com>
2023-05-31 22:04:27 -0500

    Añadir configuracion para CORS

--- BackEnd/gestionnotas/target/generated-sources/annotations/com/udea/gestionnotas/model/mapeo/CursoMapperImpl.java
+++ BackEnd/gestionnotas/target/generated-sources/annotations/com/udea/gestionnotas/model/mapeo/CursoMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-31T21:39:23-0500",
+    date = "2023-05-31T22:02:19-0500",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.19 (Oracle Corporation)"
 )
 @Component
--- BackEnd/gestionnotas/target/generated-sources/annotations/com/udea/gestionnotas/model/mapeo/GrupoMapperImpl.java
+++ BackEnd/gestionnotas/target/generated-sources/annotations/com/udea/gestionnotas/model/mapeo/GrupoMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-31T21:39:24-0500",
+    date = "2023-05-31T22:02:18-0500",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.19 (Oracle Corporation)"
 )
 @Component
