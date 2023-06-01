e5c6aa6981edd2b2dc8c2e6e3c3a73fbb348ac12
danielloaizan01 <61980060+danielloaizan01@users.noreply.github.com>
2023-06-01 16:52:46 -0500

    Update groups.service.ts

--- client/src/app/modules/professor/services/groups.service.ts
+++ client/src/app/modules/professor/services/groups.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
 
-import { environment } from 'client/src/environments/environment';
+import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { CourseInterface } from '../interfaces/course-interface';
 import { GroupInterface } from '../interfaces/group-interface';
